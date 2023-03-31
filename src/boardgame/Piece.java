85c4f057049ac246275100ee9610fd7043e893eb
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2023-03-31 17:12:51 -0300

    created methods to return the pieces

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -2,15 +2,15 @@ package boardgame;
 
 public class Piece {
 	
-	protected Position position;			// é pq eu não quero que essa position seja visivel na camada de chadres, ela é so uma psition intermediaria da matrix
-	private Board board;
+	protected Position position;			// é pq, eu não quero que essa position seja visivel na camada de chadres, ela é so uma psition intermediaria da matrix
+	private Board board;					// serve para conhecer o taboleiro onde ela esta.
 	
 	public Piece( Board board) {
 		this.position = null;
-		this.board = board;
+		this.board = board;					// quando eu crio uma peça, essa peça tem de saber qual é o tabuleiro que ela pertence 
 	}
 	
-	protected Board getBoard() {			// para so ser visivel para camada do tabuleiro
+	protected Board getBoard() {			// para so ser visivel para camada de mesmo pacotes, o pacote taboleiro e pela subclasses de peça
 		return board;
 	}
 }
