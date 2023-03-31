269f3d7437bc670d92e880df4329f6dca1834aae
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2023-03-31 17:20:14 -0300

    creating the ChessPiece class

---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,28 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+
+/* chess piece = peça de Xadrez
+ * Peça do jogo de jadres
+ * Essa classe seria como se fosse um intermediario entre os pacotes chess e board,
+ * essa peça é uma classe que o chess pode acessar.
+ */
+
+
+public class ChessPiece extends Piece {
+	private Color color;
+	
+	
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	
+	
+}
