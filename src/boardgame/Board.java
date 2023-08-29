4b023a485106af7e9b5efa59c24077c599dd319a
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2023-08-29 10:56:43 -0300

    moving Pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -49,6 +49,18 @@ public class Board {
 		piece.position = position;											// passando a posição para a peça
 	}
 	
+	public Piece removePiece ( Position position) {
+		if ( !positionExists(position) ) {
+			throw new BoardException("Position not on the board");
+		}
+		if ( piece(position) == null ) {
+			return null;
+		}
+		Piece aux  = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
 	
 	private boolean positionExists ( int row, int column ) {				// o metodo testa se a posição existe
 		return row >= 0 && row < rows && column >= 0 && column < columns;	// Testando se a position existe
